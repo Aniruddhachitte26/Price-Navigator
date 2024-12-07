734e53536ac60a79f1cf3ac4555eb6976e122ee5
Divyans-Jemni <jemni.d@northeastern.edu>
2024-12-06 21:45:20 -0500

    Regulatory Body package

---
+++ Price Navigator/src/model/RegulatoryBody/RegulatoryBodyDirectory.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.RegulatoryBody;
+
+import java.util.ArrayList;
+import model.Personnel.Person;
+import model.RegularUser.RegularUserProfile;
+
+/**
+ *
+ * @author diviyabharrathi
+ */
+public class RegulatoryBodyDirectory {
+
+    ArrayList<RegulatoryBodyProfile> RegulatoryBodyList;
+
+    public RegulatoryBodyDirectory() {
+        RegulatoryBodyList = new ArrayList();
+
+    }
+
+    public RegulatoryBodyProfile newRegularatoryBodyProfile(Person p) {
+
+        RegulatoryBodyProfile rp = new RegulatoryBodyProfile(p);
+        RegulatoryBodyList.add(rp);
+        return rp;
+    }
+
+    public RegulatoryBodyProfile findRegularatoryBodyUser(String id) {
+
+        for (RegulatoryBodyProfile rp : RegulatoryBodyList) {
+
+            if (rp.isMatch(id)) {
+                return rp;
+            }
+        }
+            return null; //not found after going through the whole list
+         }
+
+}
---
+++ Price Navigator/src/model/RegulatoryBody/RegulatoryBodyProfile.java
@@ -0,0 +1,39 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.RegulatoryBody;
+
+import java.util.ArrayList;
+import model.OrderManagement.Order;
+import model.Personnel.Person;
+import model.Personnel.Profile;
+
+/**
+ *
+ * @author diviyabharrathi
+ */
+public class RegulatoryBodyProfile extends Profile {
+    
+    ArrayList<Order> salesorders;
+
+
+    public RegulatoryBodyProfile(Person p) {
+
+        super(p); 
+        salesorders = new ArrayList();
+
+    }
+    
+    public ArrayList<Order> getSalesOrders() {
+    return this.salesorders;
+}
+    public void addSalesOrder(Order o){
+        salesorders.add(o);
+    }
+    
+    @Override
+    public String getRole(){
+        return  "RegulatoryBodyUser";
+    }
+}
