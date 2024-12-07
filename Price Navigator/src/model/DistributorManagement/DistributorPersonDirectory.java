6f9e5c3647ec9e5c44c21e87ca453b35b3d8de2a
Aniruddha Chitte <chitte.a@northeastern.edu>
2024-12-07 09:37:14 -0500

    Distributor Management

--- Price Navigator/src/model/DistributorManagement/DistributorPersonDirectory.java
+++ Price Navigator/src/model/DistributorManagement/DistributorPersonDirectory.java
@@ -1,13 +1,46 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package model.DistributorManagement;
+
+import model.Business.Business;
+import java.util.ArrayList;
+import model.Personnel.Person;
 
 /**
  *
  * @author Aniruddha
  */
 public class DistributorPersonDirectory {
-    
+
+    Business business;
+    ArrayList<DistributorPersonProfile> distributorlist;
+
+    public DistributorPersonDirectory(Business d) {
+
+        business = d;
+        distributorlist = new ArrayList();
+
+    }
+
+    public DistributorPersonProfile newSalesPersonProfile(Person p) {
+
+        DistributorPersonProfile sp = new DistributorPersonProfile(p);
+        distributorlist.add(sp);
+        return sp;
+    }
+
+    public DistributorPersonProfile findSalesPerson(String id) {
+
+        for (DistributorPersonProfile sp : distributorlist) {
+
+            if (sp.isMatch(id)) {
+                return sp;
+            }
+        }
+            return null; //not found after going through the whole list
+         }
+
 }
